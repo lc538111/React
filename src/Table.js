18f2b34188c7929d2a0a945a0e6cc68aa50bfe50
notbrianeno <lwcolpo@gmail.com>
2018-10-30 21:45:39 -0400

    First commit

---
+++ src/Table.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Tower</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = () => {
+    return <tbody></tbody>;
+}
+
+class Table extends Component {
+    render() {
+        return (
+            <table>
+                <TableHeader />
+                <TableBody />
+            </table>
+        );
+    }
+}
+
+export default Table;
